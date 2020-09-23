722aed77424c8f5b9fdb8547bbf9b2ef1833975d
kio <kioself1986@ya.ru>
2020-09-23 08:10:03 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _22_09_20__student_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prog = { 5, 4, 5, 3, 4 };
+            int[] adm = { 4, 3, 4, 3, 4, 5 };
+            int[] dis = { 2, 3, 2, 3, 1, 3, 2 };
+            Student first = new Student();
+            Student second = new Student("Ivan", "Ivanovich", "Ivanenko", "15-B", 18, prog.Length, adm.Length, dis.Length);
+            second.AddFirstMark(prog);
+            second.AddSecondMark(adm);
+            second.AddThirdMark(dis);
+            Console.WriteLine(first.ShowInfo());
+            //Console.WriteLine("------------------------------------------------------------------------");
+            Console.WriteLine(second.ShowInfo());
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*
+Придумать класс, описывающий студента. Предусмотреть в нем следующие моменты: фамилия, имя,
+отчество, группа, возраст, массив (зубчатый) оценок по
+программированию, администрированию и дизайну.
+А также добавить методы по работе с перечисленными 
+данными: возможность установки/получения оценки, получение среднего балла по заданному предмету,
+распечатка данных о студенте. 
+ */
+namespace _22_09_20__student_list
+{
+    class Student
+    {
+        public string name { set; get; }
+        public string surname { set; get; }
+        public string fathername { set; get; }
+        public string group { set; get; }
+        public int age { set; get; }
+        int[][] marks = new int[3][];
+
+        public Student() {
+            name = "StdName";
+            surname = "StdSurname";
+            fathername = "StdFatherName";
+            group = "15-A";
+            age = 20;
+            marks[0] = new int[5];
+            marks[1] = new int[6];
+            marks[2] = new int[7];
+        }
+        public Student(string n, string sur, string fat, string grp, int a, int f, int s, int t) {
+            name = n;
+            surname = sur;
+            fathername = fat;
+            group = grp;
+            age = a;
+            marks[0] = new int[f];
+            marks[1] = new int[s];
+            marks[2] = new int[t];
+        }
+        public void AddFirstMark(int[] mark) {            
+            for (int i = 0; i < marks[0].Length; i++) {
+                marks[0][i] = mark[i];                
+            }           
+        }
+        public void AddSecondMark(int[] mark) {            
+            for (int i = 0; i < marks[1].Length; i++) {
+                marks[1][i] = mark[i];               
+            }            
+        }
+        public void AddThirdMark(int[] mark) {            
+            for (int i = 0; i < marks[2].Length; i++) {
+                marks[2][i] = mark[i];                
+            }            
+        }
+        public string GetMiddleValue(int need_l) {
+            float middle_value = 0;
+            int sum_values = 0;
+            for (int i = 0; i < marks[need_l].Length; i++) {
+                middle_value += marks[need_l][i];
+                sum_values++;
+            }
+            return Math.Round(middle_value / sum_values, 1).ToString();
+        }
+        public string ShowInfo() {
+            string extract_string = "";
+            
+            extract_string += "Name: " + name + "\nSurname: " + surname + "\nFather Name: " + fathername + "\nAge: " + age + "\nGroup: " + group + "\n";
+            extract_string += "Programming marks: ";
+            for (int i = 0; i < marks[0].Length; i++) extract_string += marks[0][i].ToString() + " ";
+            extract_string += "\t\t- Middle value: " + GetMiddleValue(0).ToString();
+            extract_string += "\nAdministrating marks: ";
+            for (int i = 0; i < marks[1].Length; i++) extract_string += marks[1][i].ToString() + " ";
+            extract_string += "\t- Middle value: " + GetMiddleValue(1).ToString();
+            extract_string += "\nDesign marks: ";
+            for (int i = 0; i < marks[2].Length; i++) extract_string += marks[2][i].ToString() + " ";
+            extract_string += "\t\t- Middle value: " + GetMiddleValue(2).ToString();
+            extract_string += "\n------------------------------------------------------------------";
+            return extract_string;
+        }
+    }
+}
